8e8db3ff7e4fefb62243e37bc38175edf675f086
mingshashan <mingshashan@gmail.com>
2022-08-31 17:32:16 +0800

    feat: learn netty example

--- src/main/java/com/mingshashan/learn/echo/EchoClient.java
+++ src/main/java/com/mingshashan/learn/echo/EchoClient.java
@@ -1,8 +1,45 @@
 package com.mingshashan.learn.echo;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
 public class EchoClient {
 
+    final static String HOST = "localhost";
+    final static int PORT = 10301;
+
     public static void main(String[] args) {
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup(1);
+
+        try {
+
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(eventLoopGroup)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline()
+                                    .addLast(new LoggingHandler(LogLevel.DEBUG))
+                                    .addLast(new EchoClientHandler());
+                        }
+                    });
+
+            ChannelFuture channelFuture = bootstrap.connect(HOST, PORT).sync();
+
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+
+        } finally {
+            eventLoopGroup.shutdownGracefully();
+        }
 
     }
 }
--- src/main/java/com/mingshashan/learn/echo/EchoServer.java
+++ src/main/java/com/mingshashan/learn/echo/EchoServer.java
@@ -1,4 +1,44 @@
 package com.mingshashan.learn.echo;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
 public class EchoServer {
+
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(1);
+
+        try {
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.DEBUG))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline channelPipeline = ch.pipeline();
+                            channelPipeline.addLast(new LoggingHandler(LogLevel.DEBUG));
+                            channelPipeline.addLast(new EchoServerHandler());
+                        }
+                    });
+            // start server
+            ChannelFuture channelFuture = serverBootstrap.bind(10301).sync();
+            // wait until the server is closed
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // shutdown all event loops to terminate all threads
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
 }
